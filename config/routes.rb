fbf3cbe9508c3e3bcce30627fb63fc038a1374e4
Cao Hoai Viet <hoaiviet@siliconstraits.vn>
2015-11-14 19:07:26 +0700

    fix UI template

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :categories do
     resources :articles
   end
-
+  root 'articles#index'
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
